eeb4b1def026b08b529ba39c3b87ea69d90b06a9
Jigme Nidup <jigmenidup8102003@gmail.com>
2026-09-18 14:52:47 +0600

    f

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: false },
+  allowedDevOrigins: ['class.jigmenidup.site'],
 };
 
 export default nextConfig;
